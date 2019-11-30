6a35c217610fae17c445a3b27cc2a18c452452cd
Evangelos <angelos.h@hotmail.com>
2019-11-30 22:34:23 +0000

    Switch to using environment variables for actual variable config content

---
+++ src/dash/config.py
@@ -0,0 +1,17 @@
+import os
+from dash.consts import Engines
+
+DATABASE_CONFIG = {
+    "engine": Engines.SQLite, 
+    "options": {
+        "filename": "playthrough.db"
+    }
+}
+
+PREFIX = os.getenv("BOT_PREFIX", "$")
+DESCRIPTION = os.getenv("BOT_DESCRIPTION", "")
+TOKEN = os.getenv("BOT_TOKEN")
+
+ADMINS = [int(admin) for admin in os.getenv("BOT_ADMINS", "").split(";")]
+MODULES = os.getenv("BOT_MODULES", "").split(";")
+STARTUP = os.getenv("BOT_STARTUP_MODULES", "").split(";")
