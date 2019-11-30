b4d66868597741164cf83dde52d298fd0ea4968d
Evangelos <angelos.h@hotmail.com>
2019-11-30 18:10:11 +0000

    Adjust alembic to determine engine at runtime from config

--- src/dash/models/alembic/env.py
+++ src/dash/models/alembic/env.py
@@ -6,11 +6,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from dash.models import ModelBase
+from dash.models import ModelBase, get_engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", str(get_engine().url))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -66,9 +68,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
